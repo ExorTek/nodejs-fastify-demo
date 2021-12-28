9db3d1c9e3c415810afbaa4bc4740127086caeaa
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2021-12-28 23:33:05 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,8 @@
+let products = [
+    {id: 1, productName: 'Drink', price: 20},
+    {id: 2, productName: 'Food', price: 43},
+    {id: 3, productName: 'Laptop', price: 10000},
+    {id: 4, productName: 'Phone', price: 5000},
+    {id: 5, productName: 'Mouse', price: 200},
+];
+module.exports = products;
